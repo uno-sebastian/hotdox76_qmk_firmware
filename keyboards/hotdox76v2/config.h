9a451b27de0cba1ef33b7303933bd1f0d3f531fa
Seabass <uno.sebastian.echo@gmail.com>
2024-10-14 23:01:00 -0400

    Update config.h
    Update hotdox76v2 Pref

--- keyboards/hotdox76v2/config.h
+++ keyboards/hotdox76v2/config.h
@@ -1,5 +1,6 @@
 // Copyright 2021 JasonRen(biu)
 // Copyright 2022 Drashna Jael're (@Drashna Jael're)
+// Copyright 2024 Sebastian Echeverry (uno-sebastian)
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
@@ -10,14 +11,17 @@
  */
 
 /* disable debug print */
-//#define NO_DEBUG
+// #define NO_DEBUG
 
 /* disable print */
-//#define NO_PRINT
+// #define NO_PRINT
 
 /* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
+// #define NO_ACTION_LAYER
+#define NO_ACTION_TAPPING
+#define NO_ACTION_ONESHOT
+
+#define NO_AUTO_SHIFT_TAB // Do not Auto Shift KC_TAB but leave Auto Shift enabled for the other special characters.
+#define NO_AUTO_SHIFT_ALPHA 
 
 #define SPLIT_TRANSACTION_IDS_KB KEYBOARD_CURRENT_ALPA_SYNC
